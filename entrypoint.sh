eed142876d0584118589cfc3c938959aa0713d1d
radyaku <radyak.uinjkt@gmail.com>
2026-01-19 00:37:23 +0700

    Auto create superuser on deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Creating admin if not exists..."
+python create_admin.py
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
