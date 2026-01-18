5b32e3fa1fa530bda406f0f651ba417ccc397fb6
radyaku <radyak.uinjkt@gmail.com>
2026-01-19 00:23:02 +0700

    Fix Railway CRSF

--- asset_management/settings.py
+++ asset_management/settings.py
@@ -18,6 +18,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # Mengizinkan akses dari semua host/IP
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.up.railway.app",
+]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 INSTALLED_APPS = [
